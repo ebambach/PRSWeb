2a1d8e61e4aa143b3112f1bd2c6ea922b4e0d6fa
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-11-10 11:05:27 -0500

    added gummy bear and unstyled filter w/buttons for Request-List

--- ng/src/app/purchase-request/purchase-request-list/purchase-request-list.component.ts
+++ ng/src/app/purchase-request/purchase-request-list/purchase-request-list.component.ts
@@ -17,8 +17,81 @@ import {User} from '../../models/User';
 export class PurchaseRequestListComponent implements OnInit {
   loggedInUser: User;
 	purchaseRequests: PurchaseRequest[];
+  //Do we want to see all purchaseRequests, or the ones with a status of 
+  //"NEW," "Review," "Rejected," or "Accepted"
+  IsAll: boolean;
+  IsNew: boolean;
+  IsReview: boolean;
+  IsRejected: boolean;
+  IsApproved: boolean;
 
-	getPurchaseRequests(): void {
+  //This code will let the other toggle methods be more
+  //elegant, by first changing all of the booleans to false,
+  //and then changing only the ones we want to true within
+  //the other toggle methods.
+  toggleFalse(){
+    this.IsAll = false;
+    this.IsNew = false;
+    this.IsReview = false;
+    this.IsRejected = false;
+    this.IsApproved = false;
+  }
+
+	toggleNew(){
+    this.toggleFalse();
+    this.IsNew = true;    
+    console.log("running toggleNew");
+    console.log("IsAll = " + this.IsAll);
+    console.log("IsNew = " + this.IsNew);
+    console.log("IsReview = " + this.IsReview);
+    console.log("IsRejected = " + this.IsRejected);
+    console.log("IsApproved = " + this.IsApproved);
+  }
+
+  toggleReview(){
+    this.toggleFalse();
+    this.IsReview = true;
+    console.log("running toggleReview");
+    console.log("running toggleNew");
+    console.log("IsAll = " + this.IsAll);
+    console.log("IsNew = " + this.IsNew);
+    console.log("IsReview = " + this.IsReview);
+    console.log("IsRejected = " + this.IsRejected);
+    console.log("IsApproved = " + this.IsApproved);
+  }
+
+  toggleRejected(){
+    this.toggleFalse();
+    this.IsRejected = true;
+    console.log("running toggleRejected");
+    console.log("running toggleNew");
+    console.log("IsAll = " + this.IsAll);
+    console.log("IsNew = " + this.IsNew);
+    console.log("IsReview = " + this.IsReview);
+    console.log("IsRejected = " + this.IsRejected);
+    console.log("IsApproved = " + this.IsApproved);
+  }
+
+  toggleApproved(){
+    this.toggleFalse();
+    this.IsApproved = true;
+    console.log("running toggleApproved");
+    console.log("running toggleNew");
+    console.log("IsAll = " + this.IsAll);
+    console.log("IsNew = " + this.IsNew);
+    console.log("IsReview = " + this.IsReview);
+    console.log("IsRejected = " + this.IsRejected);
+    console.log("IsApproved = " + this.IsApproved);
+  }
+
+  //If we want to see all of the purchaseRequests,
+  //we want all of our filter booleans to read true.
+  toggleAll(){
+    this.toggleFalse();
+    this.IsAll = true;
+  }
+
+  getPurchaseRequests(): void {
 		this.PurchaseRequestSvc.list()
 			.then(resp => {
         this.purchaseRequests = resp
@@ -34,7 +107,15 @@ export class PurchaseRequestListComponent implements OnInit {
   	} else {
   		this.loggedInUser = this.SystemSvc.getLoggedIn();
   	}
-  	this.getPurchaseRequests();
+  	this.toggleAll();
+    console.log("running toggleTrue");
+    console.log("running toggleNew");
+    console.log("IsAll = " + this.IsAll);
+    console.log("IsNew = " + this.IsNew);
+    console.log("IsReview = " + this.IsReview);
+    console.log("IsRejected = " + this.IsRejected);
+    console.log("IsApproved = " + this.IsApproved);
+    this.getPurchaseRequests();    
   }
 
 }
